c0d0598bf1c479ff3c3098d4385b991320759368
iswan8 <127138564+iswan8@users.noreply.github.com>
2023-03-15 23:04:04 +0800

    postest2

---
+++ PRAKTIKUM PBO/posstest2/src/postes2/lagi01.java
@@ -0,0 +1,15 @@
+
+package postes2;
+
+
+public class lagi01 {
+     public static void main(String[] args){
+        int id = 152;
+        String kodeMK = "INF106086";
+        String namaMK = "Pemrograman Berorientasi Objek";
+        int sks = 3;
+        String nilaiHuruf = "A-";
+        System.out.printf("| %3d | %-10s | %-50.50s | %d | %s |%n", id,kodeMK,namaMK,sks,nilaiHuruf);
+     }
+    
+}
---
+++ PRAKTIKUM PBO/postest2/src/postest2/gila01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package postest2;
+
+/**
+ *
+ * @author LENOVO-GK
+ */
+public class gila01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
